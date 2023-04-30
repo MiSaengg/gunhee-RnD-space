e4128690c79e99944a18605f9ed0707b61946f14
David Cho <gcho13@my.bcit.ca>
2023-04-30 01:14:16 -0700

    minor updates

--- dataset_process/open_ai_gpt/gpt_output.js
+++
@@ -1,6 +0,0 @@
-import { API_KEY } from "./api_key.js";
-
-// Output test using top ten common words for each genre in JSON file
-
-// Output for detection of genre based on NLP detection of phrases in text, while only given the genre titles
-
